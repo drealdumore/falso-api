3ba44f38305d33fbc1ef634de1d3b56d679ef1de
drealdumore <sireroyce1@gmail.com>
2025-04-04 12:59:27 +0100

    chore: add globalErrorHandler to server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import bodyParser from "body-parser";
+import globalErrorHandler from "./utils/errorHandler";
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,7 +12,7 @@ if (process.env.NODE_ENV === "development") {
 
 app.get("/api", (req, res) => {
   res.send(
-    "Mock API is running. Use POST /api/generate-mock to generate mock data."
+    "Mock API is running. Use POST /generate-mock to generate mock data."
   );
 });
 
@@ -20,6 +21,8 @@ app.get("/health", (req, res) => {
 });
 
 const PORT = process.env.PORT || 8000;
+
+app.use(globalErrorHandler);
 
 app.listen(PORT, () => {
   console.log(`✅ Mock API running on http://localhost:${PORT}`);
